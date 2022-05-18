ce95df014b89b17076bf5a2b0b7163cf1c069114
Arian Velayati <78826553+avelayati@users.noreply.github.com>
2022-05-18 15:59:01 -0600

    Update ODE3.m

--- ODE3.m
+++ ODE3.m
@@ -1,5 +1,5 @@
 function [yout] = ODE3(F,t0,h,tfinal,y0)
-% Euler solver for ODEs
+% Trapezoid solver for ODEs
 % F: Function, t: point, h: step size, y0:initial value 
 % To solve dy/dt = F(t,y) with y(t0) = y0
 
@@ -8,7 +8,7 @@ yout = y;
 
 for t = t0 : h : tfinal-h
     s1 = F(t,y);
-    s2 = F(t+h,y+h)
+    s2 = F(t+h,y+h*s1)
     y = y + h*(s1+s2)/2;
     yout = [yout; y]
 end
